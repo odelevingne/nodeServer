77a93cc34dc74e61eac243d87eba4a72a00a3413
Oliver Delevingne <oliver@delevingne.co.uk>
2015-02-18 15:48:41 +0000

    set up mocha casperjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,28 @@
 module.exports = function(grunt){
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    mocha_casperjs: {
+      options: {
+      },
+      files: {
+        src: ['test/**/*']
+      },
+    },
     jshint: {
-      all: ['src/*.js', 'test/*.js'],
+      all: ['src/*.js', 'test/**/*'],
     },
     watch: {
-      files: ['src/*.js', 'test/*.js'],
+      files: ['src/*.js', 'test/**/*'],
       tasks: ['jshint'],
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-casperjs');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'mocha_casperjs']);
   grunt.registerTask('hint', ['jshint']);
   grunt.registerTask('watch', ['watch']);
 
