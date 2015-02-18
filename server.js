f2b79e32d266dfd931d459cf08e9eb744eebb444
Oliver Delevingne <oliver@delevingne.co.uk>
2015-02-18 17:35:00 +0000

    localhost 3000 displays hello world

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+app.get('/', function(request, response){
+  response.send("Hello world")
+});
+
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
 });
